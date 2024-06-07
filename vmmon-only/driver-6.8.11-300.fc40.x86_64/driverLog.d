68c8c9b94c3444c600e707a636edc250ff9bb84f
Krishbin Paudel <krishbinp@outlook.com>
2024-06-07 16:31:04 +0545

    mirror of vmware modules with patches

---
+++ vmmon-only/driver-6.8.11-300.fc40.x86_64/driverLog.d
@@ -0,0 +1,5 @@
+driverLog.o: .././linux/driverLog.c .././include/driver-config.h \
+ .././include/includeCheck.h .././include/compat_version.h \
+ .././include/compat_autoconf.h .././include/compat_kernel.h \
+ .././include/compat_sched.h .././linux/driverLog.h \
+ .././include/vm_assert.h .././include/vm_basic_types.h
